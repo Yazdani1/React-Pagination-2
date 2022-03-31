ac53840e60877002a2144631e5cf1d939c5cf078
Yazdani <yaz4dani@gmail.com>
2022-03-31 13:43:16 +0300

    pagination number

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,70 @@
-import React,{useEffect,useState} from 'react'
-import Posts from './components/Posts';
+import React, { useEffect, useState } from "react";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
 const App = () => {
-
-  const [data,setData] = useState([]);
-  const [loading,setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   //pagination
 
-  const [currentPage,setCurrentPage] = useState(1);
-  const [postsPerPage,setPostPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostPerPage] = useState(5);
 
   const url = "https://jsonplaceholder.typicode.com/posts";
 
-  const loadData = ()=>{
-
-    fetch(url,{
-      method: "GET"
-    }).then(res=>res.json())
-    .then(result=>{
-
-      if(result){
-        setData(result);
-        setLoading(false);
-        console.log(result);
-      }
-
-    }).catch(err=>{
-      console.log(err);
+  const loadData = () => {
+    fetch(url, {
+      method: "GET",
     })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result) {
+          setData(result);
+          setLoading(false);
+          console.log(result);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  }
-
-  useEffect(()=>{
+  useEffect(() => {
     loadData();
-  },[]);
+  }, []);
 
   if (loading) {
     return (
       <div class="text-center my-25">
-        <h1>
-          Loading...
-        </h1>
+        <h1>Loading...</h1>
       </div>
     );
   }
 
+  //To get the last posts index position
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = data.slice(indexOfFirstPost, indexOfLastPost);
+
+  //to change page
+
+  const paginate =(pageNumber)=>{
+    setCurrentPage(pageNumber)
+  }
 
   return (
     <React.Fragment>
       <h1>Data With Pagination</h1>
-      <Posts posts={data}/>
-    
+      <Posts posts={currentPosts} />
+      <Pagination 
+      postsPerPage={postsPerPage} 
+      totalPosts={data.length} 
+      paginateClick={paginate}
+      />
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default App
+export default App;
