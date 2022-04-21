895698dfb04497db0188262028d954802edab53b
Yazdani <yaz4dani@gmail.com>
2022-04-21 19:51:17 +0300

    export excel

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "../App.css";
 import "./post.css";
+import * as XLSX from 'xlsx'
+
 const Posts = ({ posts }) => {
+  
+  const downloadExcelfile = () => {
+    var wb = XLSX.utils.book_new();
+    var ws = XLSX.utils.json_to_sheet(posts);
+
+    XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+    XLSX.writeFile(wb, "MyExcel.xlsx");
+  };
+
   return (
     <div className="container post-container">
       <div className="card export-card">
-        <span className="export-design" onClick={() => alert("Clicked here")}>
+        <span className="export-design" onClick={downloadExcelfile}>
           <h4>Export</h4>
         </span>
       </div>
