d117494cf3ecbb0b69fdf2e210301085580443a0
Yazdani <yaz4dani@gmail.com>
2022-04-28 18:53:20 +0300

    Filter Option

--- client/src/components/Multiple Item Select/CardList.js
+++ client/src/components/Multiple Item Select/CardList.js
@@ -1,18 +1,66 @@
-import React from 'react'
-import "./CardList.css"
+import React, { useState } from "react";
+import "./CardList.css";
 
 const CardList = () => {
-  return (
-   <React.Fragment>
-       <div className='container'>
-           <div className='list-items'>
-               <div className='card'>
-                   <h6>React Js</h6>
-               </div>
-           </div>
-       </div>
-   </React.Fragment>
-  )
-}
+  const data = [
+    {
+      name: "React",
+      id: 1,
+    },
+    {
+      name: "Python",
+      id: 2,
+    },
+    {
+      name: "Node js",
+      id: 3,
+    },
+    {
+        name: "Nuxt js",
+        id: 4,
+      },
+      {
+        name: "Vue js",
+        id: 5,
+      },
+      {
+        name: "Express js",
+        id: 6,
+      },
+  ];
 
-export default CardList
+  const [list, setList] = useState(data);
+
+  const handleSeletect = (id) => {
+      
+    const newList = list.filter((item) => item.id !== id);
+
+    setList(newList);
+  };
+
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="list-items">
+          <div className="row">
+            {list.map((item, index) => (
+              <>
+                <div className="col-xl-3 col-lg-3">
+                  <div
+                    key={item.id}
+                    className="card card-items"
+                    onClick={() => handleSeletect(item.id)}
+                  >
+                    <h6>{item.name}</h6>
+                  </div>
+                </div>
+              </>
+            ))}
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default CardList;
